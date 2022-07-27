b8aa7ec1cfe2fd9aaa8e04bdc52d19889c4fb359
SayujSujeev <sayujsujeev550@gmail.com>
2022-07-28 02:24:05 +0530

    implemented create user

--- app/core/models.py
+++ app/core/models.py
@@ -27,7 +27,9 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(user=self._db)
+        user.save(using=self._db)
+
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
